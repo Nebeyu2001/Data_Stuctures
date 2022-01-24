699545c992505f08ec67f02818c5310430c7e412
Nebeyu Zewdie <nebeyu.zewdie@gmail.com>
2022-01-24 13:59:39 -0600

    assignment 7 and 8

---
+++ src/Assignment7/QueueInterface.java
@@ -0,0 +1,14 @@
+package Assignment7;
+
+    public interface QueueInterface <E> {
+        boolean add(E e);
+        boolean offer(E item);
+        E remove();
+        E poll();
+        E peek();
+        E element();
+        int getSize();
+
+    }
+
+
---
+++ src/Assignment7/TestMain.java
@@ -0,0 +1,33 @@
+package Assignment7;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TestMain {
+
+        public static void main(String[] args) {
+            Queue<Customer> list = new LinkedList<>();
+            list.add(new Customer("Andrew", 2, 30.00));
+            list.add(new Customer("Teddy", 3, 45.00));
+            list.add(new Customer("Yonas", 4, 60.00));
+            list.offer(new Customer("Sammy", 4, 34.0));
+
+
+            System.out.println( "The first customer processing time is: " + processingTime(list.poll()) + " min");
+            list.poll();
+            System.out.println("The second customer Processing time: " + processingTime(list.poll()) + "min");
+//        if(list.poll().getWaitTime()==0){
+//            return 0;
+//        }else{
+//            return list.poll().getWaitTime();
+        }
+        public static double processingTime(Customer customer){
+            final double MIN_WAIT = 15;
+            if(customer.getWaitTime()<=MIN_WAIT){
+                return 15;
+            }else{
+                return customer.getWaitTime() + MIN_WAIT;
+            }
+        }
+    }
+
